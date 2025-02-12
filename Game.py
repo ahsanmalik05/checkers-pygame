8feec75e2fa682ac21cd5e8ba21e83c0ca6b6d81
ikramfa1 <faizan.ikram@mail.utoronto.ca>
2025-02-12 17:56:11 -0500

    ok

---
+++ Game.py
@@ -0,0 +1,32 @@
+WIDTH, HEIGHT = 800, 800
+FPS = 60
+
+import pygame
+class Game:
+    def init(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("Checkers")
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def run(self):
+        while self.running:
+            self.clock.tick(FPS)
+            self.handle_events()
+            self.update()
+            self.draw()
+
+        pygame.quit()
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.screen.fill((255, 255, 255))
+        pygame.display.flip()
---
+++ Move.py
@@ -0,0 +1,11 @@
+class Move:
+
+    def init(self, row: int, col: int):
+        self.row = row
+        self.col = col
+
+    def get_row(self) -> int:
+        return self.row
+
+    def get_col(self) -> int:
+        return self.col
